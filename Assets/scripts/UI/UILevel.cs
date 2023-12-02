2130a06b9499bb1f2c4c90c6dc5e1e2ed4b12202
Hexiaobai <630516913@qq.com>
2023-12-02 20:56:32 +0800

    release

--- Assets/scripts/UI/UILevel.cs
+++ Assets/scripts/UI/UILevel.cs
@@ -5,5 +5,15 @@ using UnityEngine;
 public class UILevel : MonoBehaviour
 {
     public static List<bool> Levels= new List<bool> { true, false, false, false };
+    public static int cntp=0;
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+            cntp++;
+        if(cntp==10) Levels[0]=Levels[1]=Levels[2]=Levels[3]=true;
+    }
 }
+
+
+
